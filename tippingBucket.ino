c4cfe683332bdb661d621d9fadc8cbed1fbf0456
thebluetitan001 <47161028+thebluetitan001@users.noreply.github.com>
2019-10-27 23:04:37 +1100

    Update tippingBucket.ino

--- tippingBucket.ino
+++ tippingBucket.ino
@@ -1,3 +1,10 @@
+//tipping bucket collector diamter 90mm
+//volumne of bucket .7mm //amount im mm with every tip 0.1mm //connect one wire to ground
+//connect 2nd wire to pin digital interrupt pin 2(phyiscally labelled ~3)
+
+volatile long lastRiseTimeRain = 0;   
+volatile long lastRiseTimeWind = 0;   
+
 void rainISR() {
 
   //If more than 10 ms has elapsed since the last time pin 2 went high
